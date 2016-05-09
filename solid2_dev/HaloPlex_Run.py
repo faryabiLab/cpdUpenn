1b4dbdae5ed83e84affdbe37e705b6dbeb322943
Bigdeli <BigdeliA@uphs.pennhealth.prv>
2016-05-09 11:14:52 -0400

    deduped bam file required a resort ?

--- solid2_dev/HaloPlex_Run.py
+++ solid2_dev/HaloPlex_Run.py
@@ -16,15 +16,16 @@ def sample_run(sample_name, read1, read2, read_index, index2, out_dir):
         LOG_FILE = run.out_dir + run.sample_name + '_run_error.log'
         
         #trim -> align -> deduplicate reads
-        trimmed_fqs = CPD_ETR.trim(run.adapter1, run.adapter2, run.read1, run.read2, run.out_dir)
-        trimmed_files = trimmed_fqs.split(' ')
-        trimmed_fqs = CPD_ETR.trim(run.adapter1, run.adapter2, run.read1, run.read2, run.out_dir)
-        trim_fq1 = trimmed_files[0]
-        trim_fq2 = trimmed_files[1]               
-        aligned_sam = CPD_ETR.align(trim_fq1, trim_fq2, run.frag_size)
-        bam = CPD_ETR.dedup(aligned_sam, run.index2, run.amplicon_bed)
-        bam = CPD_ETR.fix(bam, run.amplicon_bed, run.index2, run.sample_name, run.lib_name)
-        CPD_ETR.index(bam)
+        #trimmed_fqs = CPD_ETR.trim(run.adapter1, run.adapter2, run.read1, run.read2, run.out_dir)
+        #trimmed_files = trimmed_fqs.split(' ')
+        #trimmed_fqs = CPD_ETR.trim(run.adapter1, run.adapter2, run.read1, run.read2, run.out_dir)
+        #trim_fq1 = trimmed_files[0]
+        #trim_fq2 = trimmed_files[1]               
+        #aligned_sam = CPD_ETR.align(trim_fq1, trim_fq2, run.frag_size)
+        #bam = CPD_ETR.dedup(aligned_sam, run.index2, run.amplicon_bed)
+        #bam = CPD_ETR.fix(bam, run.amplicon_bed, run.index2, run.sample_name, run.lib_name)
+        bam = '/project/cpdlab/cpdUpenn/solid2_dev/HiSeqSamples/CPDV150986-35_ucsc19/CPDV150986-35.align.consensus.fix.reordered.bam'
+        #CPD_ETR.index(bam)
         CPD_ETR.flagstats(bam)
 
         #begin indel realignment
