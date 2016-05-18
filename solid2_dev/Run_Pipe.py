d65e47fce7b1b18d792610634cce6da697a98e9f
Bigdeli <BigdeliA@uphs.pennhealth.prv>
2016-05-18 16:07:15 -0400

    adding demultiplexing

--- solid2_dev/Run_Pipe.py
+++ solid2_dev/Run_Pipe.py
@@ -1,4 +1,4 @@
-import csv,sys,subprocess
+import glob,sys,subprocess
 from util import Paths
 
 def parse_sheet (sample_sheet):    
@@ -13,17 +13,34 @@ def parse_sheet (sample_sheet):
 def demultiplex(run_dir, sample_sheet):
     subprocess.Call(Paths.bcl2 + '--input-dir '+ run_dir + '/Data/Intensities/BaseCalls --output-dir ' + run_dir + 
                     ' Unaligned --sample-sheet ' + sample_sheet + ' --no-eamss --use-bases-mask Y150n,I8,Y10,Y150n --mismatches 1', shell = True)
-    subprocess.Call ("'for j in `awk -F "," '{print $3}' " + sample_sheet + " | grep -v 'Sample_ID' | sort |uniq`" + 
-                     " do zcat $j/$j*R1*gz > $j/$j.R1.fastq zcat $j/$j*R2*gz > $j/$j.R2.fastq zcat $j/$j*R3*gz > $j/$j.R3.fastq gzip $j/*fastq", shell = True)
 
+# concat fasta
+#
+# @param1 = filenames: all files to be concatenated.
+# @param2 = download_to: folder to concatenate files.
+#
+# Opens ALL files in folder and writes them to one .fasta file.
+def concat_file( db_name, download_to):
+    # create file list of all files to concatenate
+    files_in_path = download_to 
+    filenames = glob.glob(files_in_path)
+    db_path = download_to + db_name + '.vcf'
+    try:
+        #open each and write to single file
+        with open( db_path, 'w') as outfile:
+            for filename in filenames:
+                with open(filename) as infile:
+                    for line in infile:
+                        outfile.write(line)
+        outfile.close()
+    except IOError:
+        print "There was an IO error concatinating your files, check your file path and authority!"
+        
+        
 def main():
     run_dir = sys.argv[1]
     sample_sheet = run_dir + '/Data/Intensities/BaseCalls/SampleSheet.csv'
-    print sample_sheet
     sample_info = parse_sheet(sample_sheet)
-    print 'demultiplexing...'
-    #demultiplex(run_dir, sample_sheet)
-    for item in sample_info:
-        print item
+    demultiplex(run_dir, sample_sheet)
     
 main()
