33d4db50fd50d9e793f5e05db59d5de7a98cdad5
Bigdeli <BigdeliA@uphs.pennhealth.prv>
2016-05-18 17:10:02 -0400

    trying to use bcl2fastq

--- solid2_dev/Run_Pipe.py
+++ solid2_dev/Run_Pipe.py
@@ -11,7 +11,7 @@ def parse_sheet (sample_sheet):
     return run_args
     
 def demultiplex(run_dir, sample_sheet):
-    subprocess.call('module load bcl2fastq2-v02.14.01.07', shell = True)
+    subprocess.call('module load bcl2fastq2-v2.17.1.14', shell = True)
     subprocess.call('bcl2fastq --input-dir '+ run_dir + '/Data/Intensities/BaseCalls --output-dir ' + run_dir + 
                     ' Unaligned --sample-sheet ' + sample_sheet + ' --no-eamss --use-bases-mask Y150n,I8,Y10,Y150n --mismatches 1', shell = True)
 
