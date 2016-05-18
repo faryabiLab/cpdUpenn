74d410fe8929d448cbe018ae134389379d61e65d
Bigdeli <BigdeliA@uphs.pennhealth.prv>
2016-05-18 18:19:12 -0400

    hopefully this works

--- solid2_dev/util/Paths.py
+++ solid2_dev/util/Paths.py
@@ -36,7 +36,7 @@ FilterAlignScore = '/project/cpdlab/Scripts/PipelineV2.1.1_hpc/FilterAlignScore.
 
 
 #demultiplex
-bcl2 = '/project/cpdlab/Tools/CASAVA_v1.8.2/src/perl/bin/configureBclToFastq.pl'
+bcl2 = '/home/bigdelia/bcl2fastq/src/perl/bin/configureBclToFastq.pl'
 
 
 ## Databases
