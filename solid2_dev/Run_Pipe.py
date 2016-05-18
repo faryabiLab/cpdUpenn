5241a40ca4a00d84dcde4ad2bddc62f81e242f2c
Bigdeli <BigdeliA@uphs.pennhealth.prv>
2016-05-18 18:59:30 -0400

    typo

--- solid2_dev/Run_Pipe.py
+++ solid2_dev/Run_Pipe.py
@@ -15,7 +15,7 @@ def demultiplex(run_dir, sample_sheet):
                     '/Unaligned --sample-sheet ' + sample_sheet + ' --no-eamss --use-bases-mask Y150n,I8,Y10,Y150n --mismatches 1', shell = True)
 
 def concat_fastq(run_dir):
-    unaligned = run_dir + '/Data/Intensities/BaseCalls/Unaligned/'
+    unaligned = run_dir + '/Unaligned/'
     subprocess.call ('cd ' + unaligned, shell = False )
     subprocess.call("for j in `awk -F "," '{print $3}' ../../SampleSheet.csv | grep -v 'Sample_ID' | sort |uniq` \
                      do \
